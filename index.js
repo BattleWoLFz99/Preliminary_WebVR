3c67ab5734323baea1deae750c59ae739e1be03f
Wesley <chqzy99@gmail.com>
2021-04-07 04:44:52 -0700

    One Object

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var canvas = document.getElementById("renderCanvas");
-var engine = new BABYLON.Engine(canvas, true);
-
 var createScene = function () {
 
 	// This creates a basic Babylon Scene object (non-mesh)
@@ -11,28 +8,74 @@ var createScene = function () {
 	camera.setTarget(BABYLON.Vector3.Zero());
 	var light = new BABYLON.DirectionalLight("light", new BABYLON.Vector3(0, -0.5, 1.0), scene);
 	light.position = new BABYLON.Vector3(0, 15, -6);
-	
+    // enable physics in the scene
+    scene.enablePhysics(new BABYLON.Vector3(0,-1,0), new BABYLON.AmmoJSPlugin());
 	// Create default environment
 	var environment = scene.createDefaultEnvironment({
 		skyboxSize: 300,
 		groundSize: 200
 	});
 	environment.setMainColor(new BABYLON.Color3(0.1, 0.3, 0.5));
-	
+    // Creat a variable to hold our score.
+    var score = 0;
+
 	//Create a sphere for the scene
 	var sphere = BABYLON.Mesh.CreateIcoSphere("sphere", {radius:0.8, flat:true, subdivisions: 16}, scene);
 	sphere.material = new BABYLON.StandardMaterial("material", scene);
 	sphere.material.diffuseColor = new BABYLON.Color3(0.588, 0.805, 0.896);
-	
+    sphere.position.y = 2;
+    sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.7 }, scene);
+    
+    // Create a button to restart the game
+    var plane = BABYLON.Mesh.CreatePlane("plane", 40);
+    plane.position.set(0, 2, 10);
+    var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(plane);
+    var button = BABYLON.GUI.Button.CreateSimpleButton("button", "Start Game");
+    button.width = 0.25;
+    button.height = "40px";
+    button.color = "white";
+    button.background = "black";
+    button.onPointerUpObservable.add(function () {
+        resetGame();
+    });
+    advancedTexture.addControl(button);
+
+    //add an event that fires when a click happens
+    scene.onPointerObservable.add((e)=>{
+        if(e.type == BABYLON.PointerEventTypes.POINTERDOWN){
+            if(e.pickInfo.pickedMesh == sphere){
+                fadeSphere(sphere);
+            }
+        }
+    });	
+
+    
+    //add a function that scales and fades the sphere
+    function fadeSphere(clickedSphere){
+        BABYLON.Animation.CreateAndStartAnimation("sphereScaleDown", clickedSphere, "scaling.x", 30, 10, 1, 0.5, 0);
+        BABYLON.Animation.CreateAndStartAnimation("sphereScaleDown", clickedSphere, "scaling.y", 30, 10, 1, 0.5, 0);
+        BABYLON.Animation.CreateAndStartAnimation("sphereScaleDown", clickedSphere, "scaling.z", 30, 10, 1, 0.5, 0);
+        BABYLON.Animation.CreateAndStartAnimation("sphereVisAnim", clickedSphere, "visibility", 30, 10, 1, 0, 0);
+        score++;
+        button.textBlock.text = "Reset Game (Score: "+score+")";
+    };
+
+    function resetGame(){
+        button.textBlock.text = "Reset Game";
+        sphere.visibility = 1;
+        sphere.scaling.x = 1;
+        sphere.scaling.y = 1;
+        sphere.scaling.z = 1;
+        sphere.position.y = 2;
+        sphere.physicsImpostor.setLinearVelocity(new BABYLON.Vector3());
+        score = 0;
+    };
+
+
 	// Add vr
 	var helper = scene.createDefaultVRExperience({createDeviceOrientationCamera: false})
 	helper.enableInteractions()
 	
+
     return scene;
 };
-
-var scene = createScene();
-
-engine.runRenderLoop(() => {
-scene.render();
-});
